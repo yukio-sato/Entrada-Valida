ad14d90c8b44af975d4627acd85788fcfcbff6f0
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-06-05 18:51:21 -0300

    [05/06/2023]Atividade

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿string answer;
+int converted_answer;
+void dialog(string txt, int cooldown)
+{
+for (int i = 0; i < txt.Length; i++)
+{
+    Console.Write(txt[i]);
+    Thread.Sleep(cooldown);
+}
+}
+void loop()
+{
+dialog("Escolha um numero entre 1 a 9: ",25);
+answer = Console.ReadLine()!;
+if (int.TryParse(answer, out converted_answer))
+{
+converted_answer = int.Parse(answer);
+if (converted_answer == 0) 
+{
+
+}
+}
+else
+{
+    loop();
+}
+}
+loop();
