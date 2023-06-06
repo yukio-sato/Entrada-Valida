f1214db4bceb31fe1697945f83562c4207e7d93f
yukio-sato <yukioutiyamasato@gmail.com>
2023-06-05 21:24:08 -0300

    [05/06/2023]Coloração do console e Bug Fixs

--- Program.cs
+++ Program.cs
@@ -10,14 +10,38 @@ for (int i = 0; i < txt.Length; i++)
 }
 void loop()
 {
-dialog("Escolha um numero entre 1 a 9: ",25);
+Console.ForegroundColor = ConsoleColor.White;
+dialog("Escolha um numero entre ",25);
+Console.ForegroundColor = ConsoleColor.Yellow;
+dialog("1 ",25);
+Console.ForegroundColor = ConsoleColor.White;
+dialog("a ",25);
+Console.ForegroundColor = ConsoleColor.Yellow;
+dialog("9 ",25);
+Console.ForegroundColor = ConsoleColor.Red;
+dialog("(0 = Cancelar)",25);
+Console.ForegroundColor = ConsoleColor.White;
+dialog(": ",25);
+Console.ForegroundColor = ConsoleColor.Yellow;
 answer = Console.ReadLine()!;
 if (int.TryParse(answer, out converted_answer))
 {
 converted_answer = int.Parse(answer);
 if (converted_answer == 0) 
 {
-
+Console.ForegroundColor = ConsoleColor.DarkRed;
+dialog("Operação Cancelada!\n",25);
+}
+else if (converted_answer >= 1 && converted_answer <= 9) 
+{
+Console.ForegroundColor = ConsoleColor.Cyan;
+dialog("Número escolhido: ",25);
+Console.ForegroundColor = ConsoleColor.Green;
+dialog($"{converted_answer}\n",25);
+}
+else
+{
+    loop();
 }
 }
 else
@@ -26,3 +50,6 @@ else
 }
 }
 loop();
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+dialog("\n\nVolte Sempre!\n",25);
+Console.ResetColor();
